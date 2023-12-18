87f20577bade7dc22232d84040d40bf19a2b5006
Paweł Wacławiak <p.waclawiak@samorzad.p.lodz.pl>
2023-12-18 17:44:07 +0100

    Complete overhaul to OOP; Movements working;

--- at_at_code/at_at_code.ino
+++ at_at_code/at_at_code.ino
@@ -1,45 +1,23 @@
-#include <Dynamixel2Arduino.h>
-#include <math.h>
 #include "headers.h"
 // #include <Ramp.h>
-
-#define DXL_SERIAL Serial1
-#define DEBUG_SERIAL Serial
 
 //This namespace is required to use Control table item names
 using namespace ControlTableItem;
 
-// Bluetooth communication buffer
-String messageBuffer = "";
 
-//initiating the motors as numbers
-const uint8_t DXL_ID1 = 1;  //motor 1 (lower part)
-const uint8_t DXL_ID2 = 2;  //motor 2 (upper part)
-const uint8_t DXL_ID3 = 3;  //motor 1 (lower part)
-const uint8_t DXL_ID4 = 4;  //motor 2 (upper part)
-const uint8_t DXL_ID5 = 5;  //motor 1 (lower part)
-const uint8_t DXL_ID6 = 6;  //motor 2 (upper part)
-const uint8_t DXL_ID7 = 7;  //motor 1 (lower part)
-const uint8_t DXL_ID8 = 8;  //motor 2 (upper part)
-const float DXL_PROTOCOL_VERSION = 2.0;
+Coordinates standPosition = Coordinates(0, -240);
+Coordinates readyPosition = Coordinates(0, -230);
+Coordinates frontPosition = Coordinates(-50, -230);
+Coordinates backPosition = Coordinates(50, -230);
+Coordinates turnFrontPosition = Coordinates(-20, -230);
+Coordinates turnBackPosition = Coordinates(20, -230);
 
-//fixed length of robot legs (later used)
-double LEG_LENGTH = 120;  //fixed length leg
-
-Dynamixel2Arduino dxl(DXL_SERIAL);
-
-double x_desired, y_desired, q1d_l, q2d_l, q1d_r, q2d_r;
-int walking_state = 4;
-bool flag_var = false;
-unsigned long tf = millis() + 500;
-
-
-int legsOrder[4] = { 1, 2, 3, 4 };  // thanks to this list we can try different setups of leg movement order
-int legOrderId = 0;
-
-int typeOfMovement = 1;    // 0: none; 1: forward; 2: backward; 3: left; 4: right;
-int whichLegIsMoving = 1;  // 0: none; 1: front-left; 2: front-right; 3: back-left; 4: back-right;
-int stepDuration = 500;
+Leg legs[4] = {
+  Leg(1, readyPosition, readyPosition, 0),  // [0] -> 1: front-left
+  Leg(2, readyPosition, readyPosition, 0),  // [1] -> 2: front-right
+  Leg(3, readyPosition, readyPosition, 0),  // [2] -> 3: back-left
+  Leg(4, readyPosition, readyPosition, 0),  // [3] -> 4: back-right
+};
 
 
 //configuring the motors and stuff for pawel
@@ -53,7 +31,6 @@ void setup() {
   dxl.begin(57600);
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  Serial.println("Hello");
 
   for (int i = 1; i <= 8; ++i) {
     // Check if the motor with ID i responds to ping
@@ -67,35 +44,31 @@ void setup() {
 void RobotStartingPosition() {
   LegAngles angles;
 
+  delay(2000);
   Coordinates coords_stand = Coordinates(0, -240);
-  angles = singleLegAngles(coords_stand, false);
-  for (int legId : legsOrder) {
-    testMoveSingleLeg(legId, angles);
+  for (Leg &leg : legs) {
+    leg.setToPosition(coords_stand);
   }
   delay(2000);
 
   Coordinates coords_ready = Coordinates(0, -230);
-  for (int legId : legsOrder) {
-    bool invertedAngles = ((legId + 1) % 2);
-    angles = singleLegAngles(coords_ready, invertedAngles);
-    testMoveSingleLeg(legId, angles);
+  for (Leg &leg : legs) {
+    leg.setToPosition(coords_ready);
   }
   delay(1000);
 }
 
-
-// TODO: What do these lines do? If not important then remove.
-bool servoDiscovery(int servoId) {
+bool servoDiscovery(int servoId) {  // TODO: What do these lines do? If not important then remove.
   dxl.torqueOff(servoId);
   dxl.setOperatingMode(servoId, OP_CURRENT_BASED_POSITION);
-  //dxl.writeControlTableItem(CURRENT_LIMIT, i, 100);
 
   //dxl.writeControlTableItem(POSITION_P_GAIN, i, 900);
   //dxl.writeControlTableItem(POSITION_I_GAIN, i, 100);
   //dxl.writeControlTableItem(POSITION_D_GAIN, i, 200);
-  dxl.writeControlTableItem(DRIVE_MODE, servoId, 2);              //drive mode 2 for the trapezoid input (see more info in the pdf of the motors) also for the rest
-  dxl.writeControlTableItem(PROFILE_VELOCITY, servoId, 500);      //trajectory time in ms
-  dxl.writeControlTableItem(PROFILE_ACCELERATION, servoId, 100);  //acceleraction time in ms
+
+  dxl.writeControlTableItem(DRIVE_MODE, servoId, 2);                   //drive mode 2 for the trapezoid input (see more info in the pdf of the motors) also for the rest
+  dxl.writeControlTableItem(PROFILE_VELOCITY, servoId, stepDuration);  //trajectory time in ms
+  dxl.writeControlTableItem(PROFILE_ACCELERATION, servoId, 100);       //acceleraction time in ms
 
   dxl.torqueOn(servoId);
   dxl.setGoalCurrent(servoId, 200, UNIT_MILLI_AMPERE);
@@ -103,73 +76,55 @@ bool servoDiscovery(int servoId) {
 
 
 
-// bool moveLeg(int legId, int position[2]) {
-//   Serial.print("");
-//   // return true/false to indicate if the movement has finished
-// }
-
-
-
-double getParabolaY(double x) {
-  return (-0.012 * (x*x)) + 30;
-}
-
-// do not remove start and stop - it will be usefull for making turns
-Coordinates getMovementTrajectory(long startTime, Coordinates start, Coordinates stop, int duration, bool isLeading) {
-  long currentTime = millis();
-  double multiplicationFactor = (currentTime - startTime) / duration;
-
-  Coordinates vector = stop-start;
-  vector.multiply(multiplicationFactor);
-
-  Coordinates newCoordinates = start + vector;
-  if (isLeading) {
-    newCoordinates.y = getParabolaY(newCoordinates.x);
-  }
-
-  return newCoordinates;
-}
-
-
-LegAngles angles;
-Coordinates coords[8] = {
-  Coordinates(0, -200),
-  Coordinates(-51, -230),
-  Coordinates(-34, -230),
-  Coordinates(-17, -230),
-  Coordinates(0, -230),
-  Coordinates(17, -230),
-  Coordinates(34, -230),
-  Coordinates(51, -230),
-};
-
-int coordinateId = -1;
 void loop() {
-  if (++coordinateId >= sizeof(coords)) {
-    coordinateId = 0;
+  bool leadingLegUpdated = false;
+  if (typeOfMovement != 0 ) {
+    for (Leg &leg : legs) {
+      delay(10);
+      if (updatedInPreviousLoop && (leg.legId == leaderUpdatingLegId)) {
+        updatedInPreviousLoop = false;
+      }
+      if (millis() > leg.movementFinishTime) {
+        if (!leadingLegUpdated && !updatedInPreviousLoop) {
+          leadingLegUpdated = true;
+          updatedInPreviousLoop = true;
+          leaderUpdatingLegId = leg.legId;
+          legOrderId = (legOrderId + 1) % 4;
+          leadingLegId = legsOrder[legOrderId];
+        }
+        leg.updateMovementParameters();
+      }
+      leg.continueMovement();
+
+      if (leg.legId == 1) {
+        bool isLeading = (leg.legId == leadingLegId);
+        float fullMovementTime = (stepDuration + (2 * stepDuration * !isLeading));
+        Coordinates movementVector = (leg.goalPosition - leg.startPosition);
+
+        // Serial.print("LegId: ");
+        // Serial.print(leg.legId);
+        // Serial.print(" >>> ");
+        // leg.startPosition.print(false);
+        // Serial.print(" -> ");
+        // leg.goalPosition.print(false);
+        // Serial.print(" => ");
+        // movementVector.print(false);
+        // Serial.print(" =/= ");
+        // leg.getCurrentLegPosition().print(false);
+        // Serial.print(" || Lead: ");
+        // Serial.print(isLeading);
+        // Serial.print("(");
+        // Serial.print(leadingLegId);
+        // Serial.print("); Rem: ");
+        // Serial.print(leg.getRemainingTime());
+        // Serial.print(" / ");
+        // Serial.print(fullMovementTime);
+        // Serial.println(" |;");
+      }
+    }
   }
 
-  // NEW CODE
-  // if (typeOfMovement > 0) {
-  //   bool movementFinished = true;
-  //   // bool movementFinished = moveLeg();
-  //   if (movementFinished) {
-  //     if (++legOrderId >= sizeof(legsOrder))
-  //       legOrderId = 0;
-  //     whichLegIsMoving = legsOrder[legOrderId];
-  //   }
-  // }
-  //================================================
-
-
-  // Naive test for all 4 legs (without taking order into account)
-  for (int legId = 0; legId < 4; legId++) {
-    int coordsId = (coordinateId + 2*legId) % 8;
-    bool invertAngles = (legId + 1) % 2;  // gives 0 for left side and 1 for right side
-    angles = singleLegAngles(coords[coordsId], invertAngles);
-    testMoveSingleLeg(legId, angles);
-    delay(1000);
-  }
+  printToSerialPlotter();
 
   // Bluetooth signals handling
   while (Serial2.available()) {
@@ -177,17 +132,51 @@ void loop() {
     messageBuffer += inputChar;
     delay(2);
 
-    // Serial.print(messageBuffer + "  |  ");
     if (inputChar == ';') {
-      Serial.println(messageBuffer);
+      // Serial.println(messageBuffer);
+      // Serial2.println(messageBuffer);
       dumpDataSerial2();
       if (messageBuffer == "LED ON;") {
         digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-        Serial2.print("LED ON;");
       }
       if (messageBuffer == "LED OFF;") {
         digitalWrite(LED_BUILTIN, LOW);  // turn the LED off (LOW is the voltage level)
-        Serial2.print("LED OFF;");
+      }
+      if (messageBuffer == "GO FORWARD;") {
+        typeOfMovement = 1;
+        forwardTurn = 0;
+        actionStartTime = millis();
+        discoverLegPositions();
+      }
+      if (messageBuffer == "GO BACKWARD;") {
+        typeOfMovement = 2;
+        forwardTurn = 0;
+        actionStartTime = millis();
+        discoverLegPositions();
+      }
+      if (messageBuffer == "TURN LEFT;") {
+        typeOfMovement = 3;
+        forwardTurn = -1;
+        actionStartTime = millis();
+        discoverLegPositions();
+      }
+      if (messageBuffer == "TURN RIGHT;") {
+        typeOfMovement = 4;
+        forwardTurn = 1;
+        actionStartTime = millis();
+        discoverLegPositions();
+      }
+      if (messageBuffer == "BULL MODE;") {
+        typeOfMovement = 1111;
+        forwardTurn = 0;
+        actionStartTime = millis();
+      }
+      if (messageBuffer == "STOP;") {
+        typeOfMovement = 0;
+      }
+      if (messageBuffer == "RESET;") {
+        typeOfMovement = 0;
+        setup();
       }
       break;
     }
@@ -195,29 +184,7 @@ void loop() {
   messageBuffer = "";
 }
 
-LegAngles singleLegAngles(Coordinates coords, bool inverted) {
-  double C_factor = 3.8298;
-  double q1, q2;
-  int invertionFactor = 1;
-  LegAngles angles;
-
-  if (inverted) {
-    invertionFactor = -1;
-  }
-
-  q2 = acos((coords.x * coords.x + coords.y * coords.y - LEG_LENGTH * LEG_LENGTH - LEG_LENGTH * LEG_LENGTH) / (2 * LEG_LENGTH * LEG_LENGTH));
-  q1 = atan2(coords.y, coords.x) - atan2(LEG_LENGTH * sin(q2), LEG_LENGTH + LEG_LENGTH * cos(q2));
-
-  angles.upperAngle = 180 - (invertionFactor * ((90 + degrees(q1)) * C_factor));
-  angles.lowerAngle = 180 - (invertionFactor * ((90 + degrees(q1) + degrees(q2)) * C_factor));
-
-  return angles;
-}
-
-void testMoveSingleLeg(int legId, LegAngles legAngles) {
-  dxl.setGoalPosition(2 * legId - 1, legAngles.upperAngle, UNIT_DEGREE);
-  dxl.setGoalPosition(2 * legId, legAngles.lowerAngle, UNIT_DEGREE);
-}
+//####====----====####====----====####====----====####====----====####====----====####====----====####====----====####
 
 // Debugging bluetooth signals using serial monitor
 void dumpDataSerial2() {
@@ -225,3 +192,98 @@ void dumpDataSerial2() {
 }
 
 
+void printToSerialPlotter() {
+  Serial.print(dxl.getPresentPosition(3,UNIT_DEGREE));
+  Serial.print(",");
+  Serial.print(dxl.readControlTableItem(POSITION_TRAJECTORY,3)*360/4096);
+  Serial.print(",");
+  Serial.print(dxl.readControlTableItem(GOAL_POSITION,3)*360/4096);
+  Serial.print(",");
+  Serial.println(dxl.getPresentCurrent(3,UNIT_MILLI_AMPERE));
+}
+
+
+void discoverLegPositions() {
+  Coordinates fullMovement, currentPosition, remainingMovement;
+  float leaderElapsedTime;
+
+  for (Leg &leg : legs) {
+    bool isLeading = (leg.legId == leadingLegId);
+
+    if (typeOfMovement == 1) { // go forward
+      if (isLeading) {
+        leg.startPosition = backPosition;
+        leg.goalPosition = frontPosition;
+      } else {
+        leg.startPosition = frontPosition;
+        leg.goalPosition = backPosition;
+      }
+    }
+    if (typeOfMovement == 2) { // go backward
+      if (isLeading) {
+        leg.startPosition = frontPosition;
+        leg.goalPosition = backPosition;
+      } else {
+        leg.startPosition = backPosition;
+        leg.goalPosition = frontPosition;
+      }
+    }
+    if ((typeOfMovement == 3) || (typeOfMovement == 4)) {  // turn left OR turn right
+      bool isTurning = (((typeOfMovement == 3) && ((leg.legId % 2) == 1)) || ((typeOfMovement == 4) && ((leg.legId % 2) == 0)));
+
+      if (isTurning) {
+        if (isLeading) {
+          leg.startPosition = turnBackPosition;
+          leg.goalPosition = turnFrontPosition;
+        } else {
+          leg.startPosition = turnFrontPosition;
+          leg.goalPosition = turnBackPosition;
+        }
+      } else {
+        if (isLeading) {
+          leg.startPosition = backPosition;
+          leg.goalPosition = frontPosition;
+        } else {
+          leg.startPosition = frontPosition;
+          leg.goalPosition = backPosition;
+        }
+      }
+    }
+
+    if (isLeading) {
+      fullMovement = leg.goalPosition - leg.startPosition;
+      currentPosition = leg.getCurrentLegPosition();
+      remainingMovement = leg.goalPosition - currentPosition;
+
+      leaderElapsedTime = ((remainingMovement.x / fullMovement.x) * stepDuration);
+      float baseMovementTime = stepDuration;
+      float remainingMovementTime = baseMovementTime - leaderElapsedTime;
+      leg.movementFinishTime = actionStartTime + remainingMovementTime;
+    }
+  }
+
+  for (Leg &leg : legs) {
+    float baseMovementTime, remainingMovementTime;
+    bool isLeading = (leg.legId == leadingLegId);
+
+    if (!isLeading) {
+      baseMovementTime = stepDuration * howManyLegsUntilLeader(leg.legId);
+      remainingMovementTime = baseMovementTime - leaderElapsedTime;
+      // float baseMovementTime = stepDuration * howManyLegsUntilLeader(leg.legId);
+      // float remainingMovementTime = baseMovementTime - leaderElapsedTime;
+      leg.movementFinishTime = actionStartTime + remainingMovementTime;
+    } else {
+      baseMovementTime = stepDuration;
+      remainingMovementTime = baseMovementTime - leaderElapsedTime;
+    }
+
+    // Serial.print("LegId: ");
+    // Serial.print(leg.legId);
+    // Serial.print(" | baseTime: ");
+    // Serial.print(baseMovementTime);
+    // Serial.print(" | remainingTime: ");
+    // Serial.print(remainingMovementTime);
+    // Serial.print(" | leading: ");
+    // Serial.println(isLeading ? "true" : "false");
+  }
+}
--- at_at_code/headers.h
+++ at_at_code/headers.h
@@ -1,13 +1,74 @@
+/////////////////////////////////////// ---=== INCLUDES ===--- ////////////////////////////////////////
+#include <Dynamixel2Arduino.h>
+#include <math.h>
+
+#define DXL_SERIAL Serial1
+#define DEBUG_SERIAL Serial
+
+/////////////////////////////////////// ---=== CONSTANTS ===--- ///////////////////////////////////////
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+// Initiating the motors as numbers
+const uint8_t DXL_ID1 = 1;  //motor 1 (upper part)
+const uint8_t DXL_ID2 = 2;  //motor 2 (lower part)
+const uint8_t DXL_ID3 = 3;  //motor 1 (upper part)
+const uint8_t DXL_ID4 = 4;  //motor 2 (lower part)
+const uint8_t DXL_ID5 = 5;  //motor 1 (upper part)
+const uint8_t DXL_ID6 = 6;  //motor 2 (lower part)
+const uint8_t DXL_ID7 = 7;  //motor 1 (upper part)
+const uint8_t DXL_ID8 = 8;  //motor 2 (lower part)
+
+float LEG_LENGTH = 120;  //fixed length leg
+int stepDuration = 500;
+
+Dynamixel2Arduino dxl(DXL_SERIAL);
+
+//////////////////////////////////// ---=== GLOBAL VARIABLES ===--- ///////////////////////////////////
+
+int legsOrder[4] = { 1, 3, 2, 4 };  // thanks to this list we can try different setups of leg movement order
+int legOrderId = 0;
+int leadingLegId = legsOrder[legOrderId];  // 0: none; 1: front-left; 2: front-right; 3: back-left; 4: back-right;
+int typeOfMovement = 0;    // 0: none; 1: forward; 2: backward; 3: left; 4: right;
+float actionStartTime;
+int leaderUpdatingLegId;
+bool updatedInPreviousLoop = false;
+float turningRate = 0.5;  // between <0; 1> where 0.0 does not move legs at all and 1.0 is going straight
+float forwardTurn = 0;  // -1: turn left; 0: go straight; 1: turn right;
+String messageBuffer = "";
+
+/////////////////////////////////////// ---=== FUNCTIONS ===--- ///////////////////////////////////////
+
+float getParabolaY(float x, float maxX) {
+  float a = (-30 / (maxX * maxX));
+  return (a * (x*x)) - 200;
+}
+
+int howManyLegsUntilLeader(int legId) {
+  int remainingLegs = 0;
+  int index;
+  while (true) {
+    index = (legOrderId + remainingLegs) % 4;
+    if (legsOrder[index] == legId) {
+      return remainingLegs;
+    }
+    ++remainingLegs;
+  }
+}
+
+//////////////////////////////////// ---=== DATA STRUCTURES ===--- ////////////////////////////////////
+
 struct LegAngles {
-  double upperAngle;
-  double lowerAngle;
+  float upperAngle;
+  float lowerAngle;
 };
 
-struct Coordinates {
-  double x;
-  double y;
+//-----------------------------------------------------------------------------------------------------
 
-  Coordinates(double x_, double y_) {
+struct Coordinates {
+  float x;
+  float y;
+
+  Coordinates(float x_, float y_) {
     x = x_;
     y = y_;
   };
@@ -36,8 +97,168 @@ struct Coordinates {
     this->y += other.y;
   }
 
-  void multiply(double factor) {
-    this->x *= factor;
-    this->y *= factor;
+  Coordinates multiply(float factor) {
+    Coordinates result;
+    result.x = this->x * factor;
+    result.y = this->y * factor;
+    return result;
+  }
+
+  Coordinates multiplyX(float factor) {
+    Coordinates result;
+    result.x = this->x * factor;
+    return result;
+  }
+  
+  void print(bool newline) {
+    Serial.print("(");
+    Serial.print(x);
+    Serial.print(", ");
+    Serial.print(y);
+    Serial.print(")");
+    if (newline) {
+      Serial.println();
+    }
   }
 };
+
+//-----------------------------------------------------------------------------------------------------
+
+struct Leg {
+  int legId;
+  
+  Coordinates startPosition;
+  Coordinates goalPosition;
+
+  float fullMovementTime;
+  float movementFinishTime;
+  bool invertedAngles;
+  bool previousLeader;
+
+  Leg(int legId, Coordinates startPosition, Coordinates goalPosition, float movementFinishTime) {
+    this->legId = legId;
+    this->startPosition = startPosition;
+    this->goalPosition = goalPosition;
+    this->movementFinishTime = movementFinishTime;
+    this->invertedAngles = ((legId % 2) == 0);
+    this->previousLeader = (legId == leadingLegId);
+  }
+  
+
+  void updateMovementParameters() {
+    bool isLeading = (this->legId == leadingLegId);
+    if (previousLeader != isLeading) {
+      // Serial.print(legId);
+      // Serial.print(" ==> ");
+      // startPosition.print(false);
+      // Serial.print("->");
+      // goalPosition.print(false);
+      // Serial.print(" --> ");
+      Coordinates temp = startPosition;
+      startPosition = goalPosition;
+      goalPosition = temp;
+      // startPosition.print(false);
+      // Serial.print("->");
+      // goalPosition.print(true);
+
+    }
+    previousLeader = isLeading;
+    if (isLeading) {
+      fullMovementTime = stepDuration;
+    } else {
+      fullMovementTime = (3 * stepDuration);
+    }
+    movementFinishTime += fullMovementTime;
+    // Serial.print("LegId: ");
+    // Serial.print(legId);
+    // Serial.print(" | Movement time: ");
+    // Serial.print(fullMovementTime);
+    // Serial.print(" | Leading: ");
+    // Serial.println(isLeading);
+
+  }
+
+  // if not used, then delete
+  float getRemainingTime() {
+    return movementFinishTime - millis();
+  }
+
+
+  Coordinates getNextLegPosition() {
+    bool isLeading = (legId == leadingLegId);
+    float fullMovementTime = (stepDuration + (2 * stepDuration * !isLeading));
+
+    float remainingTime = movementFinishTime - millis();
+    float requiredMovementPercent = 1 - (remainingTime / fullMovementTime);
+
+    Coordinates movementVector = (goalPosition - startPosition);
+    movementVector = movementVector.multiply(requiredMovementPercent);
+
+    Coordinates newPosition = startPosition + movementVector;
+
+    if (isLeading) {
+      newPosition.y = getParabolaY(newPosition.x, goalPosition.x);
+    }
+    return newPosition;
+  }
+
+
+  LegAngles calculateNewAngles(Coordinates newPosition) {
+    float C_factor = 3.8298;
+    float q1, q2;
+    int invertionFactor = 1;
+    LegAngles angles;
+
+    if (invertedAngles) {
+      invertionFactor = -1;
+    }
+
+    q2 = acos((newPosition.x * newPosition.x + newPosition.y * newPosition.y - 2 * (LEG_LENGTH * LEG_LENGTH)) / (2 * LEG_LENGTH * LEG_LENGTH));
+    q1 = atan2(newPosition.y, newPosition.x) - atan2(LEG_LENGTH * sin(q2), LEG_LENGTH + LEG_LENGTH * cos(q2));
+
+    angles.upperAngle = 180 - (invertionFactor * ((90 + degrees(q1)) * C_factor));
+    angles.lowerAngle = 180 - (invertionFactor * ((90 + degrees(q1) + degrees(q2)) * C_factor));
+    return angles;
+  }
+
+
+  void setToPosition(Coordinates newPosition) {
+    LegAngles newAngles = calculateNewAngles(newPosition);
+
+    dxl.setGoalPosition(2 * legId - 1, newAngles.upperAngle, UNIT_DEGREE);
+    dxl.setGoalPosition(2 * legId, newAngles.lowerAngle, UNIT_DEGREE);
+  }
+
+
+  void continueMovement() {
+    Coordinates newPosition = getNextLegPosition();
+    setToPosition(newPosition);
+  }
+  
+
+  // Gets current leg position from angles reading of servos
+  Coordinates getCurrentLegPosition() {
+    float C_factor = 3.8298;
+    bool inverted = ((legId % 2) == 0);
+
+    // Dynamixel2Arduino dxl(Serial1, DXL_DIR_PIN);
+    float lowerMotorAngle = dxl.getPresentPosition(2 * legId - 1, UNIT_DEGREE); //use angle in degree
+    float upperMotorAngle = dxl.getPresentPosition(2 * legId, UNIT_DEGREE);
+
+    int invertionFactor = 1;
+    if (inverted) {
+      invertionFactor = -1;
+    }
+      
+    float q1 = radians((180 - upperMotorAngle) / (invertionFactor * C_factor) - 90);
+    float q2 = radians((180 - lowerMotorAngle) / (invertionFactor * C_factor) - 90) - q1;
+
+    float x = LEG_LENGTH * (cos(q1) + cos(q1 + q2));
+    float y = LEG_LENGTH * (sin(q1) + sin(q1 + q2));
+
+    return Coordinates(x, y);
+  }
+  
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
